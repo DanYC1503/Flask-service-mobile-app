707f9d6ea114c2c0c9d9c2a19676b60421a4db72
Alexander Salazar <crepy27@gmail.com>
2025-06-01 21:16:55 -0500

    change of URL in the app

--- UI_Filter_APP/app/src/main/java/com/example/ui_filter_app/RetrofitClient.java
+++ UI_Filter_APP/app/src/main/java/com/example/ui_filter_app/RetrofitClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.3.66:8080/";
+    private static final String BASE_URL = "http://192.168.3.70:8080/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(Context context) {
